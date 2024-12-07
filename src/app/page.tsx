aba15e70d1c9c0ae0db72f6c60e1908f95d17a33
Samyam412 <kcsamy50@gmail.com>
2024-12-07 16:47:13 +0545

    ui: added shadcn and some basic changes

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,14 +1,13 @@
+import { HeroNav } from "~/styles/components/navbar";
+
 const HomePage = () => {
-  return (
-    <div
-      className="grid h-screen w-full place-items-center
-    bg-gradient-to-r from-slate-900 via-purple-900
-    to-slate-900 text-3xl font-bold text-gray-200
-  "
-    >
-      Comming soon...
-    </div>
-  );
+    return (
+        <div
+            className="w-full h-full " >
+            <HeroNav />
+        </div>
+
+    );
 };
 
 export default HomePage;
