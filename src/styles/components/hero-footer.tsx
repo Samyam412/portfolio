5dff9c90a96b16aca703739ccd594a9a6d02f387
Samyam412 <kcsamy50@gmail.com>
2024-12-08 13:37:01 +0545

    bug:simple bug

--- src/styles/components/hero-footer.tsx
+++ src/styles/components/hero-footer.tsx
@@ -4,7 +4,6 @@ import { type ReactNode } from "react";
 import AnimatedDock from "./dock";
 import { CalendarPlus, Github, Linkedin, Mail } from "lucide-react";
 import { Discord } from "./icons/discord";
-import { useToast } from "~/hooks/use-toast";
 
 export interface DockItems {
     title: string
@@ -21,19 +20,7 @@ const DockItems: DockItems[] = [
 ]
 
 export function HeroFooter() {
-    const { toast } = useToast()
-    function copyText(text: string) {
-        navigator.clipboard.writeText(text)
-        toast({ description: "Email copied to clipboard" })
-    }
-
-
     return <div className=" border-t border-t-slate-700 h-16 flex items-center justify-between px-6 text-sm  ">
-        {/* <h1>Design, Code, Test</h1> */}
-        {/* <div className="hidden md:flex gap-2 md:items-center hover:text-orange-400 transition-colors duration-300 " onClick={() => copyText("samyamkatwalwork@gmail.com")}> */}
-        {/*     <span><Mail /></span> */}
-        {/*     samyamkatwalwork@gmail.com */}
-        {/* </div> */}
         <AnimatedDock items={DockItems} />
     </div>
 }
